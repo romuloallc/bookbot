1a293980903cd9dccb00755593c130e7fdabc839
romuloallc <romulo.allc@gmail.com>
2025-10-15 22:31:06 -0300

    Using argv

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import sys
 from stats import count_words, count_chars, sort_dict, report
 
 def main():
-   report()
+   if len(sys.argv) == 1:
+       sys.exit("Usage: python3 main.py <path_to_book>")
+
+   report(sys.argv[1])
 
 main()
--- stats.py
+++ stats.py
@@ -1,20 +1,20 @@
-def get_book_text(path):
-    with open(path) as f:
+def get_book_text(book_path):
+    with open(book_path) as f:
         file_contents = f.read()
 
     return file_contents
 
 
-def count_words():
+def count_words(book_path):
     num_words = 0
-    text = get_book_text("./books/frankenstein.txt")
+    text = get_book_text(book_path)
     for word in text.split():
         num_words += 1
 
     return num_words
 
-def count_chars():
-    text = get_book_text("./books/frankenstein.txt").lower()
+def count_chars(book_path):
+    text = get_book_text(book_path).lower()
     char_count = {}
     for word in text.split():
         for char in word:
@@ -25,8 +25,8 @@ def count_chars():
 def sort_on(items):
     return items['num']
             
-def sort_dict():
-    text_dict = count_chars()
+def sort_dict(book_path):
+    text_dict = count_chars(book_path)
     list_dict = []
     for k, v in text_dict.items():
         list_dict.append({"char": k, "num": v})
@@ -34,14 +34,14 @@ def sort_dict():
     list_dict.sort(reverse=True, key=sort_on)
     return list_dict
 
-def report():
-    sorted_dict = sort_dict()
+def report(book_path):
+    sorted_dict = sort_dict(book_path)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
-    print(f"Found {count_words()} total words")
+    print(f"Found {count_words(book_path)} total words")
     print("--------- Character Count -------")
-    list_dict = sort_dict()
+    list_dict = sort_dict(book_path)
     for item in list_dict:
         if item['char'].isalpha(): 
             print(f"{item['char']}: {item['num']}")
